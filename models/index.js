c4688b70938d4b7128d6cc184bf10c6aae77c9f6
63csmith <63csmith@gmail.com>
2022-06-15 17:49:28 -0600

    update 1.1

--- models/index.js
+++ models/index.js
@@ -14,9 +14,19 @@ Category.hasMany(Product, {
   onDelete: "CASCADE",
 });
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+});
 module.exports = {
   Product,
   Category,
